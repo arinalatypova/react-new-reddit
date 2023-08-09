57ba83ca70198773972264403c3e3d447c021462
Латыпова Арина <arinalatypova26@gmail.com>
2023-08-09 14:14:00 +0500

    Конфигурация webpack на клиенте

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import { Header } from './Header';
+
+window.addEventListener('load', () => {
+  ReactDom.render(<Header />, document.getElementById('react_root'));
+});
