746d1e311d54ac7bac6c5df2afba373236df831b
Латыпова Арина <arinalatypova26@gmail.com>
2023-08-09 16:04:42 +0500

    Конфигурация webpack на сервере

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,4 @@
-const path = require('path');
-const HTMLWebpackPlagin = require('html-webpack-plugin');
+const clientConfig = require('./cfg/webpack.client.config');
+const serverConfig = require('./cfg/webpack.server.config');
 
-const NODE_ENV = process.env.NODE_ENV;
-const IS_DEV = NODE_ENV === 'development';
-const IS_PROD = NODE_ENV === 'production';
-
-function setupDevtool() {
-  if (IS_DEV) return 'eval';
-  if (IS_PROD) return false;
-}
-
-module.exports = {
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-  mode: NODE_ENV ? NODE_ENV : 'development',
-  entry: path.resolve(__dirname, 'src/index.jsx'), // откуда начинать
-  output: {
-    // куда складывать транспилированные пакеты
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/, // регулярное выражение, в котором описано какие файлы с помощью лодера будут обработаны
-        use: ['ts-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HTMLWebpackPlagin({
-      template: path.resolve(__dirname, 'index.html'),
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: IS_DEV,
-  },
-  devtool: setupDevtool(),
-};
+module.exports = [clientConfig, serverConfig];
