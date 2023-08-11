0389c78d4864498be78e80861aa5a6a48a01f373
Латыпова Арина <arinalatypova26@gmail.com>
2023-08-11 12:14:07 +0500

    Пример Typescript

--- dist/client/client.js
+++ dist/client/client.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7417ad6eb1f08d7af989";
+/******/ 	var hotCurrentHash = "b6eaa4cd624e0a9a3e73";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
