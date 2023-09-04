259c93a4c1359caebf3fb19aceab178b3dca0d50
Латыпова Арина <arinalatypova26@gmail.com>
2023-09-04 11:19:21 +0500

    Utils

--- src/utils/react/stopPropagation.tsx
+++ src/utils/react/stopPropagation.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export function preventDefault<T extends (e: any) => void>(fn: T) {
+export function stopPropagation<T extends (e: any) => void>(fn: T) {
   return <E extends React.SyntheticEvent<any>>(e: E) => {
-    e.preventDefault();
+    e.stopPropagation();
     fn(e);
   };
 }
